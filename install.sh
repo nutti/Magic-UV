56e78893d2c2e7c2155308a8b46e4b5d42f18a81
Nutti <nutti.metro@gmail.com>
2017-01-02 00:09:44 +0900

    Add feature: Preserve UV Aspect

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rm -rf ${HOME}/Library/Application\ Support/Blender/2.75/scripts/addons/uv_magic_uv 
-cp -r uv_magic_uv ${HOME}/Library/Application\ Support/Blender/2.75/scripts/addons/
+rm -rf ${HOME}/Library/Application\ Support/Blender/2.77/scripts/addons/uv_magic_uv 
+cp -r uv_magic_uv ${HOME}/Library/Application\ Support/Blender/2.77/scripts/addons/
--- uv_magic_uv/__init__.py
+++ uv_magic_uv/__init__.py
@@ -56,6 +56,7 @@ if "bpy" in locals():
     imp.reload(muv_mirroruv_ops)
     imp.reload(muv_wsuv_ops)
     imp.reload(muv_unwrapconst_ops)
+    imp.reload(muv_preserve_uv_aspect)
 else:
     from . import muv_preferences
     from . import muv_menu
@@ -73,6 +74,7 @@ else:
     from . import muv_mirroruv_ops
     from . import muv_wsuv_ops
     from . import muv_unwrapconst_ops
+    from . import muv_preserve_uv_aspect
 
 import bpy
 
@@ -87,6 +89,7 @@ def view3d_uvmap_menu_fn(self, context):
     self.layout.operator(muv_mirroruv_ops.MUV_MirrorUV.bl_idname, icon="PLUGIN")
     self.layout.menu(muv_menu.MUV_WSUVMenu.bl_idname, icon="PLUGIN")
     self.layout.operator(muv_unwrapconst_ops.MUV_UnwrapConstraint.bl_idname, icon='PLUGIN')
+    self.layout.menu(muv_preserve_uv_aspect.MUV_PreserveUVAspectMenu.bl_idname, icon='PLUGIN')
 
 
 def image_uvs_menu_fn(self, context):
