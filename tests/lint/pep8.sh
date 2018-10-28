1b62ca97e510ef849f06262e2cad022434407804
Nutti <nutti.metro@gmail.com>
2018-10-28 20:25:58 +0900

    Update: CI environment about lint

--- tests/lint/pep8.sh
+++ tests/lint/pep8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -ne 1 ]; then
-    echo "Usage: check_code_style.sh <target>"
+    echo "Usage: pep8.sh <target>"
     exit 1
 fi
 
@@ -9,27 +9,6 @@ tgt=${1}
 
 files=`find ${tgt} -name "*.py"`
 ignores=("__init__.py")
-
-# pylint
-for file in ${files[@]}; do
-    # ignore file in ignores
-    found=0
-    for ign in ${ignores[@]}; do
-        if [ `echo "${file}" | grep "${ign}"` ]; then
-            found=1
-        fi
-    done
-    if [ ${found} -eq 1 ]; then
-        continue
-    fi
-    echo "======= pylint test "${file}" ======="
-    pylint ${file}
-    ret=`echo $?`
-    if [ ${ret} -ne 0 ]; then
-        echo "Test failed (error code: "${ret}")"
-        exit 1
-    fi
-done
 
 # pep8
 for file in ${files[@]}; do
