e6688f709c7603fb4d7a65dcc8db7618ae869742
MatthiasThDs <50454126+MatthiasThDs@users.noreply.github.com>
2020-10-19 13:24:19 +0200

    Update: Validate context if there is any of the requested space types available (#138)

--- src/magic_uv/op/world_scale_uv.py
+++ src/magic_uv/op/world_scale_uv.py
@@ -52,10 +52,7 @@ def _is_valid_context_for_measure(context):
         return False
 
     # only 'VIEW_3D' space is allowed to execute
-    for space in context.area.spaces:
-        if space.type == 'VIEW_3D':
-            break
-    else:
+    if not common.is_valid_space(context, ['VIEW_3D']):
         return False
 
     return True
@@ -71,10 +68,7 @@ def _is_valid_context_for_apply(context):
         return False
 
     # only 'VIEW_3D' space is allowed to execute
-    for space in context.area.spaces:
-        if space.type == 'VIEW_3D':
-            break
-    else:
+    if not common.is_valid_space(context, ['VIEW_3D']):
         return False
 
     return True
