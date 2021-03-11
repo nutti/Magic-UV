615dfdc47a5134948297195b3bdc8e205e15f9ac
nutti <nutti.metro@gmail.com>
2021-03-11 13:57:59 +0900

    Fix (Align UV Cursor): Failed to align the cursor when 'UV Sync Selection' is on (#148)

--- src/magic_uv/op/align_uv_cursor.py
+++ src/magic_uv/op/align_uv_cursor.py
@@ -191,7 +191,8 @@ class MUV_OT_AlignUVCursor(bpy.types.Operator):
                     uv_layer = bm.loops.layers.uv.verify()
 
                     for f in bm.faces:
-                        if not f.select:
+                        if (not context.tool_settings.use_uv_select_sync and
+                                not f.select):
                             continue
                         for l in f.loops:
                             uv = l[uv_layer].uv
@@ -220,18 +221,30 @@ class MUV_OT_AlignUVCursor(bpy.types.Operator):
                         return None
                     uv_layer = bm.loops.layers.uv.verify()
 
-                    for f in bm.faces:
-                        if not f.select:
-                            continue
-                        for l in f.loops:
-                            if not l[uv_layer].select:
+                    if context.tool_settings.use_uv_select_sync:
+                        for v in bm.verts:
+                            if not v.select:
                                 continue
-                            uv = l[uv_layer].uv
-                            max_.x = max(max_.x, uv.x)
-                            max_.y = max(max_.y, uv.y)
-                            min_.x = min(min_.x, uv.x)
-                            min_.y = min(min_.y, uv.y)
-                            no_selected_face = False
+                            for l in v.link_loops:
+                                uv = l[uv_layer].uv
+                                max_.x = max(max_.x, uv.x)
+                                max_.y = max(max_.y, uv.y)
+                                min_.x = min(min_.x, uv.x)
+                                min_.y = min(min_.y, uv.y)
+                                no_selected_face = False
+                    else:
+                        for f in bm.faces:
+                            if not f.select:
+                                continue
+                            for l in f.loops:
+                                if not l[uv_layer].select:
+                                    continue
+                                uv = l[uv_layer].uv
+                                max_.x = max(max_.x, uv.x)
+                                max_.y = max(max_.y, uv.y)
+                                min_.x = min(min_.x, uv.x)
+                                min_.y = min(min_.y, uv.y)
+                                no_selected_face = False
             if no_selected_face:
                 max_ = Vector((1.0, 1.0))
                 min_ = Vector((0.0, 0.0))
